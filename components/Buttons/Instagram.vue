900f9dc57293d9917dbbcf3ee5f847328e512218
Devlin Kros <devlin.kros.dk@gmail.com>
2024-10-15 16:18:40 +0200

    clean slate

--- components/Buttons/Instagram.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <Button :logo="instagram" heading="test"
-        styling="bg-gradient-to-b from-indigo-900 via-pink-600 to-[#fdfd66] text-white" svg-styling="text-white"
-        header-text="Contact via" header-title="Instagram" footer-text="@livecrowdnl" link="https://ig.me/m/livecrowdnl" />
-</template>
-
-<script setup>
-import instagram from '~/assets/img/instagram.svg'
-</script>
--- composables/useCraftureFavicon.ts
+++
@@ -1,9 +0,0 @@
-import { useHead } from '#app'
-
-export const useCraftureFavicon = () => {
-  useHead({
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/Crafture_favicon_black.png' },
-    ]
-  })
-}
